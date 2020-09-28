38cfa4b443417862b9e7ec93c881678a1e93e9c7
Georgiy21 <georgiy.voropayev@gmail.com>
2020-09-27 19:29:42 -0700

    Added SEO title. Added classify_produtc_1

--- add_product_listings.py
+++ add_product_listings.py
@@ -15,7 +15,7 @@ columns = ['Handle', 'Title', 'Body (HTML)', 'Vendor', 'Type', 'Tags', 'Publishe
            'Google Shopping / Google Product Category', 'Google Shopping / Gender', 'Google Shopping / Age Group', 'Google Shopping / MPN', 'Google Shopping / AdWords Grouping', 'Google Shopping / AdWords Labels', 'Google Shopping / Condition', 'Google Shopping / Custom Product', 'Google Shopping / Custom Label 0', 'Google Shopping / Custom Label 1', 'Google Shopping / Custom Label 2', 'Google Shopping / Custom Label 3', 'Google Shopping / Custom Label 4', 'Variant Image', 'Variant Weight Unit', 'Variant Tax Code', 'Cost per items']
 
 info_fields = ['Model #', 'Image 1', 'Image 2', 'Image 3', 'Image 4', 'Image 5', 'Image 6', 'Image 7', 'Image 8', 'Image 9', 'Image 10', 'Image 11',
-               'Image 12', 'Product Name', 'Features', 'Brand', 'Total Weight', 'Delivered Cost CA', 'Cost', 'UPC Code', 'Color', 'Category #1', 'Category #2']
+               'Image 12', 'Product Name', 'Features', 'Brand', 'Total Weight', 'Delivered Cost CA', 'Depth', 'Width', 'Height', 'Cost', 'UPC Code', 'Color', 'Category #1', 'Category #2']
            
 vendor = 'Pending - Bestar'
 published = 'FALSE'
@@ -80,43 +80,6 @@ def preprocess_file(file_p, file_m):
 
     return price_list
 
-#! Change the range of products here
-# decide whether product has varients
-def classify_product(sorted_list):
-    model = sorted_list[67]['Model #'].strip()
-
-    return_list = []
-    group_list = []
-    temp_prod = ''
-
-    for product in sorted_list[67:77]:
-        p_model = product['Model #'].split('-')[0].strip()
-
-        # if left part of model of the product match with model
-        if p_model == model.split('-')[0]:
-            # if it's the same product
-            if product['Model #'].strip() == model:
-                temp_prod = product
-            # if it's not the same product, but belongs to the same group
-            else:
-                group_list.append(temp_prod)
-                temp_prod = product
-        # if left part of model of the product doesn't match with model
-        else:
-            if not group_list:
-                return_list.append([temp_prod])
-                
-            else:
-                group_list.append(temp_prod)
-                return_list.append(group_list)
-                group_list = []
-            model = product['Model #'].strip()
-                
-            temp_prod = product
-
-
-    return return_list
-
 # Get Product Name, replace space and --- with a single - to get handle
 # For variants of product, have one handles for each variant
 # Get title of the product. If variants, get modified title
@@ -151,7 +114,7 @@ def get_img_alt_text(items):
     img_alt_text = []
 
     for product in items:
-        img_alt_text.append(product['Product Name'].replace('Bestar', '').strip())
+        img_alt_text.append(product['Product Name'].replace('Bestar', 'Modubox').strip())
     
    
     return img_alt_text
@@ -165,15 +128,12 @@ def get_text(product):
     complete_sentences = []
     for k, v in product.items():
         if k == 'Features':
-            # print(v,'\n')
             text = v
 
     x = re.split(r'(([A-Za-z])[.!?)])', text)
-  
     
     for sent in range(len(x)):
         try:
-            # print(x[sent])
             if len(x[sent]) > 2:
                 if len(x[sent + 1]) == 2:
                     sentence = x[sent].lstrip('.').strip()
@@ -266,22 +226,19 @@ def categorize_text(text):
     for d in dimensions:
         index = dimensions.index(d)
         dim = ''
-        if 'Interior dimensions' in d:
-            dim = d.split('are')[1].strip().replace(" ","").replace('x', ' x ')
-            dimensions.remove(d)
-            dimensions.insert(index, dim)
-        elif ', commercial' in d:
+        if ', commercial' in d:
             dim = d.split(', commercial')[0].strip().replace(" ","").replace('x', ' x ')
             dimensions.remove(d)
-            dimensions.insert(index, dim)
         elif 'mattresses' in d or 'mattress' in d:
             pass
-
+        elif 'Dimensions:' in d:
+            dim = d.replace('Dimensions: ', '').replace(" ","").replace('x', ' x ').replace(':',': ')
+            dimensions.remove(d)
         else:     
             dim = d.replace(" ","").replace('x', ' x ').replace(':',': ')
             dimensions.remove(d)
-            dimensions.insert(index, dim)
-        
+        dimensions.insert(index, dim)
+
     # print('Specifications: ' , specifications)
     # print("\nDescription: " , description)
     # print('\nDimensions: ' , dimensions)
@@ -292,9 +249,6 @@ def categorize_text(text):
 def get_body_html(pl, p_type):
 
     body = ''
-    # structure = '<h4>Description</h4>\n' + '<p>Items Included</p>\n' + '<ul>\n' + '<li>...</li>\n' + '</ul>\n' + '<meta charset="utf-8">\n' + \
-    #     '<p><span>...</span></p>\n' + '<h4>Dimensions</h4>\n' + '<p>...</p>\n' + \
-    #     '<h4>Specifications</h4>\n' + '<ul>\n' + '<li>...</li>\n' + '</ul>'
 
     for product in pl:
         text = get_text(product)
@@ -308,19 +262,50 @@ def get_body_html(pl, p_type):
         for row in description:
             desc += row
 
-        body += '<h4>Description</h4>\n' + '<p>Items Included</p>\n'
-        body += '<ul>\n' + '<li>' + p_type + '</li>\n' + \
-            '</ul>\n' + '<meta charset="utf-8">\n'
+        body += '<h4>Description</h4>\n' + '<p>Items Included</p>\n<ul>\n'
+
+        if dimensions:
+            item_included = ''
+            for x in dimensions:
+                if ':' in x and ('Overall dimensions:' not in x or 'Overalldimensions:' not in x or 'Interiordimensionsofthedrawersare' not in x):
+                    item_included = x.split(':')[0].strip()
+                    body += '<li>' + item_included + '</li>\n'
+            if item_included == '':
+                body += '<li>' + p_type + '</li>\n'
+        else:
+            body += '<li>' + p_type + '</li>\n'
+        body += '</ul>\n' + '<meta charset="utf-8">\n'
         body += '<p><span>' + desc + '</span></p>\n'
         
     else:
         pass
+    
+    # Get dimensions from columns 'Width' 'Depth' 'Height' 
+    # Do not include if v == 'See features'
+    dim_sent = ''
+    for product in pl:
+        if dim_sent == '':
+            for k,v in product.items():
+                if k == 'Width' and v != 'See features':
+                    dim_sent += v + '"W'
+                elif k == 'Depth' and v != 'See features':
+                    dim_sent += ' x ' + v + '"D'
+                elif k == 'Height' and v != 'See features':
+                    dim_sent += ' x ' + v + '"H'
+        else:
+            continue
+ 
 
     # Append sentences in DIMENSIONS list to body IF EXIST
-    if dimensions:
-        body += '<h4>Dimensions</h4>\n'
-        for row in dimensions:
+    if dimensions or dim_sent:
+        body += '<h4>Dimensions</h4>\n'   
+        for row in dimensions: 
             body += '<p>' + row + ' ' + '</p>\n'
+
+        if dimensions and 'Overalldimensions:' in body:
+            pass
+        elif not dimensions or 'Interiordimensionsofthedrawersare' in body or 'Interiordimensionsofdrawerare' in body:
+            body += '<p>' + 'Overall Dimensions: ' + dim_sent + '</p>\n'
 
     # Append sentences in SPECIFICATIONS list to body IF EXIST
     if specifications:
@@ -352,7 +337,7 @@ def get_body_html(pl, p_type):
 # Get tags of each product and append it to listw
 def gen_tags(pl):
 
-    product_tags = ['Bestar', 'Brand_Modubox', 'Made In_Canada']
+    product_tags = ['Bestar', 'Brand_Modubox', 'Made In_Canada', 'Style_Contemporary']
 
     for product in pl:
         for k, v in product.items():
@@ -370,11 +355,23 @@ def gen_tags(pl):
                     tag1 = 'Colour_' + v.strip()
                     product_tags.append(tag1)
             elif k == 'Category #1':
+                # print("'",v,"'")
+                # if v in (None, ""):
+                #     continue
                 if v == 'Desks' and 'Type_Desk' not in product_tags:
                     product_tags.append('Type_Desk')
+                # elif 'Office' in v and 'Room_Home Office' not in product_tags:
+                #     product_tags.append('Room_Home Office')
+                
             elif k == 'Category #2':
-                if v == 'Desk Sets' and 'Room_Home Office' not in product_tags:
+                # if not v:
+                #     continue
+                if 'Desk' in v and 'Room_Home Office' not in product_tags:
                     product_tags.append('Room_Home Office')
+                elif 'Office ' in v and 'Type_Desk' not in product_tags:
+                    product_tags.append('Type_Desk')
+            else:
+                continue
 
     return product_tags
 
@@ -463,6 +460,15 @@ def get_option_name_value(pl):
 
     return product_list
 
+def get_seo_title(title):
+
+    seo_title = ''
+
+    if "-" in title:
+        seo_title = 'Modubox ' + title.rsplit(' - ')[0].strip()
+
+    return seo_title
+
 # Get the SKU of each product and append to list
 def get_sku(pl):
 
@@ -476,7 +482,6 @@ def get_sku(pl):
     
     return skus
 
-
 def get_cost_per_item(pl):
 
     costs = []
@@ -484,7 +489,7 @@ def get_cost_per_item(pl):
     for product in pl:
         for k,v in product.items():
             if k == 'Delivered Cost CA':
-                price = v.split('$')[1].strip()
+                price = v.split('$')[1].strip().replace(',','')
                 costs.append(price)
  
     return costs
@@ -557,7 +562,7 @@ def get_quantity(pl, inventory_filename):
     return stock
 
 # Generate a product line to be written to the output.csv file
-def produce_template_line(handle, skus, barcodes, title, body, option_dicts, product_type, tags, total_weights, quantity, cost_per_item, price, main_img, alt_text, img, obj, obj_num):
+def produce_template_line(seo_title, handle, skus, barcodes, title, body, option_dicts, product_type, tags, total_weights, quantity, cost_per_item, price, main_img, alt_text, img, obj, obj_num):
 
     template_header = {'Handle': '', 'Title': '', 'Body (HTML)': '',
                        'Vendor': '', 'Type': '', 'Tags': '', 'Published': '', 'Option1 Name': '',
@@ -580,7 +585,7 @@ def produce_template_line(handle, skus, barcodes, title, body, option_dicts, pro
     option = 1
 
     template_header['Handle'] = handle
-    
+   
     if obj == 0 and img == 0:
         template_header['Title'] = title
         template_header['Body (HTML)'] = body
@@ -621,7 +626,8 @@ def produce_template_line(handle, skus, barcodes, title, body, option_dicts, pro
         template_header['Image Src'] = main_img[img]
         template_header['Image Position'] = img + 1
         template_header['Image Alt Text'] = alt_text[img]
-        template_header['Cost per items'] = cost_per_item[obj]
+        template_header['SEO Title'] = seo_title
+        template_header['Cost per items'] = int(float(cost_per_item[obj]))
 
 
         new_line = template_header
@@ -639,16 +645,15 @@ def produce_template_line(handle, skus, barcodes, title, body, option_dicts, pro
             template_header['Variant Inventory Qty'] = int(float(quantity[obj]))
             template_header['Variant Inventory Policy'] = variant_inventory_policy
             template_header['Variant Fulfillment Service'] = variant_fulfillment_service
-            template_header['Variant Price'] = '0.0'
-            template_header['Variant Compare At Price'] = '0.0'
             template_header['Variant Requires Shipping'] = variant_requires_shipping
             template_header['Variant Taxable'] = variant_taxable
             template_header['Variant Barcode'] = barcodes[obj]
             template_header['Variant Weight Unit'] = weight_unit
-            template_header['Image Src'] = main_img[obj]
+
+            template_header['Image Src'] = main_img[img]
             template_header['Image Position'] = img + 1
             template_header['Image Alt Text'] = alt_text[obj]
-            template_header['Cost per items'] = cost_per_item[obj]
+            template_header['Cost per items'] = int(float(cost_per_item[obj]))
             template_header['Variant Price'] = price[0]
             template_header['Variant Compare At Price'] = price[1]
 
@@ -666,10 +671,93 @@ def produce_template_line(handle, skus, barcodes, title, body, option_dicts, pro
 
     return new_line
 
+#! Change the range of products here
+# PRODUCT WITH LETTERS FOR MODEL: decide whether product has varients
+def classify_product_1(sorted_list):
+    # print(len(sorted_list))
+    model = sorted_list[0]['Model #'].strip()
+
+    return_list = []
+    group_list = []
+    temp_prod = ''
+
+    for product in sorted_list[0:]:
+
+        p_model = product['Model #'].rsplit('-',1)[0].strip()
+
+        # if left part of model of the product match with model
+        if p_model == model.rsplit('-',1)[0]:
+
+            # if it's the same product
+            if product['Model #'].strip() == model:
+                temp_prod = product
+            
+            elif product['Model #'].split('-',0)[0].strip() == model.split('-',0)[0].strip():
+                group_list.append(temp_prod)
+                temp_prod = product
+            # if it's not the same product, but belongs to the same group
+            else:
+                group_list.append(temp_prod)
+                temp_prod = product
+        # if left part of model of the product doesn't match with model
+        else:
+            if not group_list:
+                return_list.append([temp_prod])
+                
+            else:
+                group_list.append(temp_prod)
+                return_list.append(group_list)
+                group_list = []
+            model = product['Model #'].strip()
+                
+            temp_prod = product
+
+
+    return return_list
+
+#! Change the range of products here
+# decide whether product has varients
+def classify_product(sorted_list):
+    model = sorted_list[0]['Model #'].strip()
+
+    return_list = []
+    group_list = []
+    temp_prod = ''
+
+    for product in sorted_list[0:]:
+
+        p_model = product['Model #'].split('-')[0].strip()
+
+        # if left part of model of the product match with model
+        if p_model == model.split('-')[0]:
+
+            # if it's the same product
+            if product['Model #'].strip() == model:
+                temp_prod = product
+            # if it's not the same product, but belongs to the same group
+            else:
+                group_list.append(temp_prod)
+                temp_prod = product
+        # if left part of model of the product doesn't match with model
+        else:
+            if not group_list:
+                return_list.append([temp_prod])
+                
+            else:
+                group_list.append(temp_prod)
+                return_list.append(group_list)
+                group_list = []
+            model = product['Model #'].strip()
+                
+            temp_prod = product
+
+
+    return return_list
 
 def main():
     filename_price = 'Price List -  Bestar - September 2020 - Canada.csv'
-    filename_matt = 'Price List-Bestar-Matt.csv'
+    filename_matt = 'Price List-Bestar-Matt-1.csv'
+    # filename_matt = 'product_import_template (many var imgs).csv'
     inventory_file = 'bestar inventory listnextdate.csv'
 
     # Return a list of new product SKUs with needed information
@@ -678,25 +766,18 @@ def main():
     # sort list of dictionaries by 'Model #'
     sorted_list = sorted(price_list, key=lambda i: i['Model #'])
 
+    # REMOVE
+    # sorted_list_1 = []
+    # for i in sorted_list:
+    #     l = []
+    #     l.append(i)
+    #     sorted_list_1.append(l)
+
     # product variant
     classified_list = classify_product(sorted_list)
-    
-    # for product in classified_list:
-    #     if product[0]['Model #'] == '110895-17': #'110887-17'
-    #         skus = get_sku(product)
-    #         print(skus)
 
-    #         text = get_text(product[0])
-    #         tags = gen_tags(product)
-    #         print(tags)
-
-    #         description, specifications, dimensions = categorize_text(text)
-
-            # print('Specifications: ' , specifications)
-            # print("\nDescription: " , description)
-            # print('\nDimensions: ' , dimensions , '\n')
-
-    with open('generated_new_Bestar_import.csv', 'w', newline='') as outputfile:
+    inc = 0
+    with open('generated_new_Bestar_import.csv', 'w', newline='', encoding='utf8') as outputfile:
         writer = csv.DictWriter(outputfile, fieldnames=columns)
         writer.writeheader()
 
@@ -705,8 +786,8 @@ def main():
         count = 1
 
         for items in classified_list:
-
-
+        # for items in sorted_list_1:
+            
             product_type = get_type(items)
             option_dicts = get_option_name_value(items)
             barcodes = get_upc(items)
@@ -714,17 +795,19 @@ def main():
 
             skus = get_sku(items)
             print(skus)
+            
             cost_per_item = get_cost_per_item(items)
             price = get_price(items, filename_matt)
 
             quantity = get_quantity(items, inventory_file)
             
             body = get_body_html(items, product_type)
+            # print(body)
             img_lists = get_image(items)       
+            # print(img_lists)
 
             tags = gen_tags(items)
             # print(tags)
-
             title, handle = get_title_handle(items)
 
             # Deal with products with the same Title and handle
@@ -738,17 +821,19 @@ def main():
                 else:
                     title_exist = title
 
-            # print(title , ' | ' , handle)
-
             alt_text = get_img_alt_text(items)
+
+            seo_title = get_seo_title(title)
+            # print(seo_title)
             obj_num = len(items)
 
             for obj in range(len(img_lists)):
                 for i in range(len(img_lists[obj])):
-                    line = produce_template_line(handle, skus, barcodes, title, body, option_dicts, product_type, tags, total_weights, quantity, cost_per_item, price[obj], img_lists[obj], alt_text, i , obj, obj_num)
+                    line = produce_template_line(seo_title, handle, skus, barcodes, title, body, option_dicts, product_type, tags, total_weights, quantity, cost_per_item, price[obj], img_lists[obj], alt_text, i , obj, obj_num)
                     writer.writerow(line)
+            inc += 1
+    print(inc)
 
 
 if __name__ == '__main__':
     main()
-
